65719c30d53b8493c4ac493b11f2c32a45757d98
abhispaceX <abhiramgaddam13@gmail.com>
2024-04-26 14:29:12 +0530

    adding to-do-app

---
+++ src/App.js
@@ -0,0 +1,26 @@
+import React, {Component} from "react"
+import TodoList from "./components/TodoList"
+import "../node_modules/bootstrap/dist/css/bootstrap.min.css";
+import GoogleLogin from "react-google-login";
+
+export class App extends Component {
+  responseGoogle=(response)=>{
+    console.log(response.profile.name)
+  }
+  render(){
+  return (
+    <>
+     <GoogleLogin
+      clientId="268887589904-9p434qrn1sn22idp7hlrjfnbd4opj88s.apps.googleusercontent.com"
+      buttonText="Login"
+      onSuccess={this.responseGoogle}
+      onFailure={this.responseGoogle}
+      cookiePolicy={'single_host_origin'}
+     />
+      <TodoList/>
+    </>
+  )
+}
+}
+
+export default App;
